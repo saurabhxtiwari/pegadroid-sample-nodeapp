e42f67f3d9d6e50659002873a3524bc7c798c65b
Saurabh Tiwari <saurabh.x.tiwari@gmail.com>
2017-08-26 22:14:09 +0530

    Included basic identity and fabric client operations

---
+++ routes.js
@@ -0,0 +1,71 @@
+/**
+*********************************************************
+Copyright (c) 2017 Pegadroid IQ Solutions Private Limited
+All rights reserved
+*********************************************************
+*/
+
+'use strict';
+
+const restify = require("restify")
+const config = require("./lib/config/restifyConfig.js")
+const identityHandler = require("./lib/identity/identityHandler.js")
+const networkHandler = require("./lib/network/networkHandler.js")
+const restifyPlugin = restify.plugins;
+
+var server = restify.createServer({
+    name : config.name,
+    version: config.version
+})
+
+server.use(restifyPlugin.jsonBodyParser({ mapParams: true }))
+server.use(restifyPlugin.acceptParser(server.acceptable))
+server.use(restifyPlugin.queryParser({ mapParams: true }))
+server.use(restifyPlugin.fullResponse())
+
+server.listen(config.port)
+
+server.get("/createUser", function(req, res, next){
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    var handler = new identityHandler()
+    handler.createUser({"enrollmentID": "testuser4", "enrollmentSecret":"password"})
+    .then(
+        (response) => {
+            res.end(JSON.stringify(response))
+            return next()
+        }
+    )
+
+    var handler = new networkHandler()
+    handler.queryChannels()
+    .then(
+        (response) => {
+            res.end(JSON.stringify(response))
+            return next()
+        }
+    )
+})
+
+server.get("/queryChannels", function(req, res, next){
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    var handler = new networkHandler()
+    handler.queryChannels()
+    .then(
+        (response) => {
+            res.end(JSON.stringify(response))
+            return next()
+        }
+    )
+})
+
+server.get("/queryChaincodes", function(req, res, next){
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    var handler = new networkHandler()
+    handler.queryChaincodes()
+    .then(
+        (response) => {
+            res.end(JSON.stringify(response))
+            return next()
+        }
+    )
+})
