0045113ce0b8fde407995f4ffd9ff5381369bb36
Saurabh Tiwari <saurabh.x.tiwari@gmail.com>
2017-08-30 15:21:54 +0530

    minor refactoring

--- routes.js
+++ routes.js
@@ -71,6 +71,30 @@ server.get("/queryChaincodes", function(req, res, next){
     )
 })
 
+server.get("/queryInstantiatedChaincodes/:channelName", function(req, res, next){
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    var handler = new networkHandler()
+    handler.queryInstantiatedChaincodes(req.params.channelName)
+    .then(
+        (response) => {
+            res.end(JSON.stringify(response))
+            return next()
+        }
+    )
+})
+
+server.get("/queryChannelInfo/:channelName", function(req, res, next){
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    var handler = new networkHandler()
+    handler.queryChannelInfo(req.params.channelName)
+    .then(
+        (response) => {
+            res.end(JSON.stringify(response))
+            return next()
+        }
+    )
+})
+
 server.get("/createChannel/:channelName", function(req, res, next){
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
     var handler = new networkHandler()
@@ -87,6 +111,48 @@ server.get("/joinChannel/:channelName", function(req, res, next){
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
     var handler = new networkHandler()
     handler.joinChannel(req.params.channelName, handler.peer)
+    .then(
+        (response) => {
+            res.end(JSON.stringify(response))
+            return next()
+        }
+    )
+})
+
+server.get("/installChaincode/:chaincodeId/:chaincodeVersion", function(req, res, next){
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    var handler = new networkHandler()
+    var peers = []
+    peers.push(handler.peer)
+    handler.installChaincode(peers, req.params.chaincodeId, req.params.chaincodeVersion)
+    .then(
+        (response) => {
+            res.end(JSON.stringify(response))
+            return next()
+        }
+    )
+})
+
+server.get("/instantiateChaincode/:channelName/:chaincodeId/:chaincodeVersion", function(req, res, next){
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    var handler = new networkHandler()
+    var peers = []
+    peers.push(handler.peer)
+    handler.instantiateChaincode(req.params.channelName, peers, req.params.chaincodeId, req.params.chaincodeVersion)
+    .then(
+        (response) => {
+            res.end(JSON.stringify(response))
+            return next()
+        }
+    )
+})
+
+server.get("/invokeChaincode/:channelName/:chaincodeId/:chaincodeVersion", function(req, res, next){
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    var handler = new networkHandler()
+    var peers = []
+    peers.push(handler.peer)
+    handler.invokeChaincode(req.params.channelName, peers, req.params.chaincodeId, req.params.chaincodeVersion)
     .then(
         (response) => {
             res.end(JSON.stringify(response))
